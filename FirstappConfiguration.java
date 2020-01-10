6a028fc1f9a5023c6ba311b7fad5f890d5565dcf
Janhavi2798 <59727970+Janhavi2798@users.noreply.github.com>
2020-01-10 18:34:46 +0530

    Add files via upload

---
+++ FirstappConfiguration.java
@@ -0,0 +1,20 @@
+package com.firstapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="com.Firtapp")
+@EnableWebMvc
+public class FirstappConfiguration {
+	@Bean
+	public ViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/jsps/", ".jsp");
+		return viewResolver;
+	}
+	
+}
